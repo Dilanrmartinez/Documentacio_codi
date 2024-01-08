723e0453ffd1fcfdea2b2a8934b0f1a086bfa5b7
Dilanrmartinez <145446194+Dilanrmartinez@users.noreply.github.com>
2024-01-08 21:38:58 +0100

    Add files via upload

---
+++ documentacio_codi.py
@@ -0,0 +1,18 @@
+class prova:
+    """Esta clase calcular la suma de los diferentes numeros enteros"""
+    
+    def suma2enteros(a,b):
+    
+        """Calcula la suma de dos numeros enteros"""
+    
+        r  = a + b
+        return "la suma es igual a: " + str(r)
+
+    def suma3enteros(a,b,c):
+        
+        """Calcula la suma de tres numeros enteros"""
+        
+        p = a + b + c
+        return "La suma es igual a: " + str(p)
+    
+help(prova)
